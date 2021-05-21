3f7f83b18994cd5b1e062c9eb3fbc31bd9fa4ccd
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-21 13:03:43 -0500

    Develop hero header w/ social icon links

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,7 +5,39 @@ import socialLinks from "../constants/social_links"
 import { StaticImage } from "gatsby-plugin-image"
 
 const Hero = () => {
-  return <h2>hero component</h2>
+  return (
+    <header className="hero">
+      <section className="section-center hero-center">
+        <article className="hero-info">
+          <div>
+            <div className="underline"></div>
+            <h1>I'm Ryan</h1>
+            <h4>freelance web and full-stack JS developer</h4>
+            <Link to="/contact" className="btn">
+              contact me
+            </Link>
+            <div className="social-links">
+              {socialLinks.map(link => {
+                return (
+                  <a href={link.url} key={link.id} className="social-link">
+                    {link.icon}
+                  </a>
+                )
+              })}
+            </div>
+          </div>
+        </article>
+        <StaticImage
+          // you don't want to control the height of a static image using an SVG as it will clip pieces, instead, control the height using the width
+          src="../assets/images/hero.svg"
+          alt="portfolio"
+          className="hero-img"
+          placeholder="blurred"
+        />
+        {/* <img src={heroImg} alt="portfolio" className="hero-img-svg" /> */}
+      </section>
+    </header>
+  )
 }
 
 export default Hero
