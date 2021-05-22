d3a1e4150656ed513960049d2d13521e91de892a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-22 15:20:24 -0500

    Develop responsive nav menu

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import Navbar from "./Navbar"
 import Sidebar from "./Sidebar"
 import Footer from "./Footer"
@@ -6,9 +6,19 @@ import Footer from "./Footer"
 import "../assets/css/main.css"
 
 const Layout = ({ children }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <>
-      <Navbar />
+      {/* we pass the toggle function to nav bar, so when state changes here, it will make its way down to the sidebar component */}
+      <Navbar toggleSidebar={toggleSidebar} />
+      {/* we'll pass the state value down to the sidebar component so the sidebar can act accordingly */}
+      {/* and toggle sidebar for the close button */}
+      <Sidebar isOpen={isOpen} toggleSidebar={toggleSidebar} />
       {children}
       <Footer />
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,14 +3,17 @@ import logo from "../assets/images/logo.svg"
 import { FaAlignRight } from "react-icons/fa"
 import pageLinks from "../constants/links"
 import { Link } from "gatsby"
-const Navbar = () => {
+
+// get that toggleSidebar function being passed down from <Layout>
+const Navbar = ({ toggleSidebar }) => {
   return (
     <nav className="navbar">
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} alt="web dev" />
           <button type="button" className="toggle-btn">
-            <FaAlignRight />
+            {/* use that toogleSidebar in a click handler */}
+            <FaAlignRight onClick={toggleSidebar} />
           </button>
         </div>
         <div className="nav-links">
