b1a160ca73884d565940eb01f0b254263939117a
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-22 13:10:29 -0500

    Build out individual page templates w/ query

--- src/pages/projects/{StrapiProject.title}.js
+++ src/pages/projects/{StrapiProject.title}.js
@@ -1,11 +1,30 @@
 import React from "react"
+import { graphql } from "gatsby"
+import Seo from "../../components/Seo"
 
-const ProjectTemplate = ({ pageContext: { title } }) => {
+const ProjectTemplate = ({ pageContext: { title }, data }) => {
   return (
-    <div>
-      <h4>{title}</h4>
-    </div>
+    <>
+      <main className="project-template-page">
+        <h2>{title}</h2>
+        <p>{data.strapiProject.description}</p>
+      </main>
+    </>
   )
 }
+
+export const query = graphql`
+  query getSingleProject($title: String) {
+    strapiProject(title: { eq: $title }) {
+      description
+      title
+      image {
+        localFile {
+          publicURL
+        }
+      }
+    }
+  }
+`
 
 export default ProjectTemplate
