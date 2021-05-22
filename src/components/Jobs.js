d4808281375edbeee62257a4394d58803cd40589
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-22 10:20:40 -0500

    Develop the jobs component w/ state handling

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -4,8 +4,70 @@ import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
 
+const query = graphql`
+  {
+    allStrapiJob(sort: { fields: created_at, order: ASC }) {
+      nodes {
+        company
+        position
+        date
+        jd_val {
+          id
+          jd_bullet
+        }
+      }
+    }
+  }
+`
+
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  // console.log(data)
+  const {
+    allStrapiJob: { nodes: jobs },
+  } = data
+
+  // state for which job is selected and being displayed
+  const [value, setValue] = React.useState(0)
+
+  const { company, date, position, jd_val } = jobs[value]
+  return (
+    <section className="section jobs">
+      <Title title="Experience" />
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-container">
+          {jobs.map((item, i) => {
+            return (
+              <button
+                key={i}
+                className={i === value ? "job-btn active-btn" : "job-btn"}
+                onClick={() => setValue(i)}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* jd container */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {jd_val.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon" /> {item.jd_bullet}
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        More Info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
