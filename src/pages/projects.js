29debdf38657547c331044991c3b1cf3abdde30e
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-22 12:45:21 -0500

    Develop /product page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -2,12 +2,49 @@ import React from "react"
 import { graphql } from "gatsby"
 import Projects from "../components/Projects"
 import Seo from "../components/Seo"
-const ProjectsPage = () => {
+const ProjectsPage = ({
+  data: {
+    allStrapiProject: { nodes: projects },
+  },
+}) => {
   return (
     <>
-      <h2>projects page</h2>
+      <main className="page">
+        <section className="projects-page">
+          <Projects title="All Projects" projects={projects} />
+        </section>
+      </main>
     </>
   )
 }
+
+export const query = graphql`
+  {
+    allStrapiProject {
+      nodes {
+        description
+        featured
+        github
+        id
+        slug
+        title
+        url
+        stack {
+          id
+          title
+        }
+        image {
+          id
+          localFile {
+            childImageSharp {
+              gatsbyImageData(layout: CONSTRAINED, placeholder: BLURRED)
+            }
+          }
+        }
+      }
+      totalCount
+    }
+  }
+`
 
 export default ProjectsPage
