35b1aab2f91878c000fc6367f83eee6a4dadb1fd
Ryan Johnson <ryanleejwebdev@gmail.com>
2021-05-22 12:55:08 -0500

    Programmatically create project stub pages

---
+++ src/pages/projects/{StrapiProject.title}.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const ProjectTemplate = ({ pageContext: { title } }) => {
+  return (
+    <div>
+      <h4>{title}</h4>
+    </div>
+  )
+}
+
+export default ProjectTemplate
